acd5b78daf4a2b89a499a06dc94162c65e6615af
Tyson <henry@henrys-mbp.home>
2024-03-14 14:42:35 -0400

    change key name and update readme

--- README.md
+++ README.md
@@ -1,14 +1,7 @@
-# terrablox
+# tetromino
 
 ## Infrastructure
 * EC2
 * S3
 * Security Group
 * *.pem file
-
-
-## Steps
-1. Installing Terraform
-2. Setting up AWS Provider for Terraform
-3. Defining AWS Resources in Terraform
-4. Deploy using Terraform
--- tetromino/web.tf
+++ tetromino/web.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "terraform_ec2" {
   ami             = "ami-01c592dda54e43c1a"
   count           = 1
-  key_name        = "My Anime Host"
+  key_name        = "cineviewhost"
   instance_type   = "t2.micro"
   security_groups = [ aws_security_group.terra_security_group.name ]
   tags= {
